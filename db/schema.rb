d3e9372a1495a3bc3be99e1efb9d5739fbba0ae7
Thibault <thibault.courrie@gmail.com>
2017-08-02 16:09:37 +0200

    user creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730221126) do
+ActiveRecord::Schema.define(version: 20170802113454) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20170730221126) do
     t.string "github_handle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
